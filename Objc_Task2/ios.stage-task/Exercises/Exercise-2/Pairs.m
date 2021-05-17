8af89f9ce4f5efada52885f55c0d0321b2828ec6
Andrzej <anjeynov@gmail.com>
2021-05-18 00:21:50 +0300

    feat: add realization - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSArray <NSNumber *>  *sortedArray = [array sortedArrayUsingSelector:@selector(compare:)];
+    NSInteger count = 0;
+    for (int i = 0; i < sortedArray.count - 1 ; i++) {
+        for (int j = 1; j < sortedArray.count; j++) {
+            if((sortedArray[j].intValue - sortedArray[i].intValue)== number.intValue) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 @end
