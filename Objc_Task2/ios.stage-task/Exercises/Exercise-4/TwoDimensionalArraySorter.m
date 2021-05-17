94123d77e02fcd8bb3dde33a26fa3ec968b248ad
Andrzej <anjeynov@gmail.com>
2021-05-18 01:28:39 +0300

    feat: add realization - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,6 +3,33 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
+    NSMutableArray<NSNumber *> *numbersArray = [NSMutableArray new];
+    NSMutableArray<NSString *> *stringsArray = [NSMutableArray new];
+    if ([array.firstObject isKindOfClass:NSArray.class]) {
+        for (NSArray *element in array) {
+            if(element.count != 0) {
+                if([element.firstObject isKindOfClass:NSString.class]) {
+                    [stringsArray addObjectsFromArray:element];
+                } else {
+                    [numbersArray addObjectsFromArray:element];
+                }
+            }
+        }
+    } else {
+        return @[];
+    }
+    [stringsArray sortUsingSelector:@selector(compare:)];
+    [numbersArray sortUsingSelector:@selector(compare:)];
+    if (stringsArray.count == 0) {
+        return numbersArray;
+    }
+    if (numbersArray.count == 0) {
+        return stringsArray;
+    }
+    NSMutableArray<NSArray* > *result = [NSMutableArray new];
+    [result addObject:numbersArray];
+    [result addObject:stringsArray];
+    
     return @[];
 }
 
